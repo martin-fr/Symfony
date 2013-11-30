c3cc368f97cdcc8e172b3e359271287b94703de0
martin-fr <martin.trx.francois@gmail.com>
2013-11-30 19:13:33 +0100

    MAJ jquery highligh accents sensitive

--- web/bundles/bootstrap/js/jquery.highlight-4.js
+++ web/bundles/bootstrap/js/jquery.highlight-4.js
@@ -14,40 +14,83 @@
 
  */
 
-jQuery.fn.highlight = function(pat) {
-    function innerHighlight(node, pat) {
-        var skip = 0;
-        if (node.nodeType == 3) {
-            var pos = node.data.toUpperCase().indexOf(pat);
-            if (pos >= 0) {
-                var spannode = document.createElement('span');
-                spannode.className = 'highlight';
-                var middlebit = node.splitText(pos);
-                var endbit = middlebit.splitText(pat.length);
-                var middleclone = middlebit.cloneNode(true);
-                spannode.appendChild(middleclone);
-                middlebit.parentNode.replaceChild(spannode, middlebit);
-                skip = 1;
-            }
-        }
-        else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
-            for (var i = 0; i < node.childNodes.length; ++i) {
-                i += innerHighlight(node.childNodes[i], pat);
-            }
-        }
-        return skip;
-    }
-    return this.length && pat && pat.length ? this.each(function() {
-        innerHighlight(this, pat.toUpperCase());
-    }) : this;
+function stripAccents(str) {
+    var rExps=[
+        {re:/[\xC0-\xC6]/g, ch:'A'},
+        {re:/[\xE0-\xE6]/g, ch:'a'},
+        {re:/[\xC8-\xCB]/g, ch:'E'},
+        {re:/[\xE8-\xEB]/g, ch:'e'},
+        {re:/[\xCC-\xCF]/g, ch:'I'},
+        {re:/[\xEC-\xEF]/g, ch:'i'},
+        {re:/[\xD2-\xD6]/g, ch:'O'},
+        {re:/[\xF2-\xF6]/g, ch:'o'},
+        {re:/[\xD9-\xDC]/g, ch:'U'},
+        {re:/[\xF9-\xFC]/g, ch:'u'},
+        {re:/[\xD1]/g, ch:'N'},
+        {re:/[\xF1]/g, ch:'n'} ];
+    for(var i=0, len=rExps.length; i<len; i++)
+        str=str.replace(rExps[i].re, rExps[i].ch);
+    return str;
 };
 
-jQuery.fn.removeHighlight = function() {
-    return this.find("span.highlight").each(function() {
-        this.parentNode.firstChild.nodeName;
-        with (this.parentNode) {
-            replaceChild(this.firstChild, this);
-            normalize();
+jQuery.extend({
+    highlight: function (node, re, nodeName, className) {
+        if (node.nodeType === 3) {
+            //var match = node.data.match(re);
+            var match = stripAccents(node.data).match(re);
+            if (match) {
+                var highlight = document.createElement(nodeName || 'span');
+                highlight.className = className || 'highlight';
+                var wordNode = node.splitText(match.index);
+                wordNode.splitText(match[0].length);
+                var wordClone = wordNode.cloneNode(true);
+                highlight.appendChild(wordClone);
+                wordNode.parentNode.replaceChild(highlight, wordNode);
+                return 1; //skip added node in parent
+            }
+        } else if ((node.nodeType === 1 && node.childNodes) && // only element nodes that have children
+            !/(script|style)/i.test(node.tagName) && // ignore script and style nodes
+            !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
+            for (var i = 0; i < node.childNodes.length; i++) {
+                i += jQuery.highlight(node.childNodes[i], re, nodeName, className);
+            }
         }
+        return 0;
+    }
+});
+
+jQuery.fn.unhighlight = function (options) {
+    var settings = { className: 'highlight', element: 'span' };
+    jQuery.extend(settings, options);
+
+    return this.find(settings.element + "." + settings.className).each(function () {
+        var parent = this.parentNode;
+        parent.replaceChild(this.firstChild, this);
+        parent.normalize();
     }).end();
+};
+
+jQuery.fn.highlight = function (words, options) {
+    var settings = { className: 'highlight', element: 'span', caseSensitive: false, wordsOnly: false };
+    jQuery.extend(settings, options);
+
+    if (words.constructor === String) {
+        words = [words];
+    }
+
+    words = jQuery.map(words, function(word, i) {
+        return stripAccents(word);
+    });
+
+    var flag = settings.caseSensitive ? "" : "i";
+    var pattern = "(" + words.join("|") + ")";
+    if (settings.wordsOnly) {
+        pattern = "\\b" + pattern + "\\b";
+    }
+
+    var re = new RegExp(pattern, flag);
+
+    return this.each(function () {
+        jQuery.highlight(this, re, settings.element, settings.className);
+    });
 };
